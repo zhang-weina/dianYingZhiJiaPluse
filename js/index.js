1f693ca76ffeb73769118fa0d5cfedb8d318527a
ZhangWeiNa <3495485886@qq.com>
2020-12-14 23:17:33 +0800

    three

--- js/index.js
+++ js/index.js
@@ -9,10 +9,66 @@ var timeId = setInterval(function () {
     }
 }, 10)
 
-//点击注册账户跳转到注册页面
+/*点击注册账户显示注册页面
+注册成功后“注册账户”变为“个人中心”*/
+var formDiv = document.getElementById("formDiv");
+var uname = document.getElementById("uname");
+var username = document.getElementById("username");
+var p1 = document.createElement("p");
+formDiv.style.display = "none";
 var login = document.getElementById("Blogin");
 login.onclick = function () {
-    window.open("demo1.html");
+    formDiv.style.display = "block";
+}
+uname.onfocus = function () {
+    p1.innerHTML = "支持中文数字";
+    p1.style.fontSize = "10px";
+    p1.style.color = "gray";
+    username.appendChild(p1);
+}
+uname.onblur = function () {
+    if (this.value == "") {
+        p1.innerHTML = "用户名不能为空";
+        p1.style.fontSize = "10px";
+        p1.style.color = "gray";
+        username.appendChild(p1);
+    } else {
+        username.removeChild(p1);
+    }
+}
+var password = document.getElementById("password");
+var pw = document.getElementById("pw");
+var p2 = document.createElement("p");
+password.onfocus = function () {
+    p2.innerHTML = "建议使用两种字符组合";
+    p2.style.fontSize = "10px";
+    p2.style.color = "gray";
+    pw.appendChild(p2);
+}
+password.onblur = function () {
+    if (this.value == "") {
+        p2.innerHTML = "密码不能为空";
+        p2.style.fontSize = "10px";
+        p2.style.color = "gray";
+        pw.appendChild(p2);
+    } else {
+        pw.removeChild(p2);
+    }
+}
+
+var sub = document.getElementById("sub");
+var form = document.getElementById("form");
+sub.onclick = function () {
+    if (uname.value != "" && password.value != "") {
+        alert("注册成功！");
+        login.innerHTML = "个人中心";
+        formDiv.style.display = "none";
+        
+    } else {
+        alert("注册失败！");
+        formDiv.style.display = "none";
+    }
+
 }
 
 //banner列表滑上，显示不同列表
@@ -40,14 +96,27 @@ for (var i = 0; i < liList.length; i++) {
     }
 }
 
-//banner图下方点击列表时  修改下方标题名字
+//banner图下方点击列表时  修改下方标题名字和图片
 var title = document.getElementById("title");
 var subul = document.getElementById("subul");
 var liList = subul.getElementsByTagName("li");
+var imgs = document.getElementById("images");
+var imgList = imgs.getElementsByTagName("img");
 for (var i = 0; i < liList.length; i++) {
     liList[i].onclick = function () {
         var titleName = this.innerHTML;
         title.innerHTML = titleName;
+        for (var j = 0; j < imgList.length; j++) {
+            var str = imgList[j].getAttribute("src");
+            var num = 0;
+            if (parseInt(str.charAt(7)) >= 6) {
+                num = parseInt(str.charAt(7)) - 5;
+            } else {
+                num = parseInt(str.charAt(7)) + 5;
+            }
+            imgList[j].setAttribute("src", "images/" + num + ".png");
+        }
+
     }
 }
 
